b3c540ae8113aa2503e4aebcd278c8744b4479b8
taten <taten.usa@gmail.com>
2020-01-16 02:36:44 -0300

    programando index, ruta creada, hoja de estilos creada

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('plantilla')
+@section('titulo')
+Crossover LATAM
+@endsection
+@section('css')
+styles
+@endsection('css')
+@section('main')
+
+    <div class="crossoverTop">
+    </div>
+asfagfafas
+@endsection('main')
---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
+    <!-- Bootstrap JS --><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS --><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS --><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS --><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Icono del logo en pestana --><link rel="icon" type="image/png" href="/favicon.ico" />
+    <title>@yield('titulo')</title>
+    <link rel="stylesheet" href="/css/@yield('css').css">
+  </head>
+  <body>
+    <header>
+      {{-- contenido --}}
+    </header>
+    <main>
+      @yield('main')
+    </main>
+    <footer>
+      {{-- contenido --}}
+    </footer>
+  </body>
+</html>
