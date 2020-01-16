829e00640a4f2f184bf31d70f08544a784e8b34d
taten <taten.usa@gmail.com>
2020-01-16 05:03:30 -0300

    crossover 0.1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,44 @@ styles
 @endsection('css')
 @section('main')
 
-    <div class="crossoverTop">
+  <div class="">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <!-- indicators
+       <ol class="carousel-indicators">
+           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+           <li data-target="#myCarousel" data-slide-to="1"></li>
+           <li data-target="#myCarousel" data-slide-to="2"></li> -->
+      </ol>
+      <div class="carousel-inner">
+        <div class="vp carousel-item active">
+          <img src="/img/CrossoverTop3.png" class="d-block w-100" alt="">
+        </div>
+        <div class="vp carousel-item">
+          <img src="/img/CrossoverTop4.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="vp carousel-item">
+          <img src="/img/CrossoverTop5.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="vp carousel-item">
+          <img src="/img/CrossoverTop2.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="vp carousel-item">
+          <img src="/img/CrossoverTop.png" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      {{-- <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a> --}}
     </div>
-asfagfafas
+  </div>
+
+
+<div class="black">
+
+</div>
 @endsection('main')
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -7,13 +7,54 @@
     <!-- Bootstrap JS --><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <!-- Bootstrap JS --><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS --><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Google Fonts --><link href="https://fonts.googleapis.com/css?family=Playfair+Display|Roboto&display=swap" rel="stylesheet">
     <!-- Icono del logo en pestana --><link rel="icon" type="image/png" href="/favicon.ico" />
     <title>@yield('titulo')</title>
     <link rel="stylesheet" href="/css/@yield('css').css">
   </head>
   <body>
     <header>
-      {{-- contenido --}}
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/"><img class="logo" src="/img/crossoverlogo.svg"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Quienes Somos <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Productos
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Human to Human</a>
+                <a class="dropdown-item" href="#">Go to Goals</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Project Team</a>
+              </div>
+            </li>
+
+
+            <li class="nav-item">
+              <a class="nav-link" href="/nosotros">Nuestro Equipo</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Clientes</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="/ayuda">Contacto</a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
     </header>
     <main>
       @yield('main')
